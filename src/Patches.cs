6ba98141ec41d54d54ce28ddb640630a3a958db7
GruffCassquatch <cassanda.agius@gmail.com>
2023-01-08 11:53:12 +1100

    Add project files.

---
+++ src/Patches.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Il2CppSystem;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace RespawnTweaker
+{
+    public enum AnimalType
+    {
+        False, Bear, Deer, Fish, Moose, Rabbit, Wolf
+    }
+
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(SpawnRegion), "Start")]
+        internal class UpdateSpawnRegion
+        {
+            private static void Postfix(SpawnRegion __instance)
+            {
+                if (Settings.settings.modFunction)
+                {
+                    if (IsSandbox())
+                    {
+                        string scene = GameManager.m_ActiveScene;
+                        if (scene == null) return;
+
+                        AnimalType animalType = GetAnimalType(__instance);
+                        if (animalType != AnimalType.False)
+                        {
+                            ApplyAnimalSettings(animalType, __instance);
+                        }
+
+
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(IceFishingHole), "Start", null)]
+        internal static class UpdateIceFishingHole
+        {
+            private static void Postfix(IceFishingHole __instance)
+            {
+                if (Settings.settings.modFunction)
+                {
+                    string scene = GameManager.m_ActiveScene;
+                    if (scene == null) return;
+
+                        if (IsSandbox())
+                        {
+                            ApplyFishSettings(__instance);
+                        }
+                    }
+                }
+            }
+
+
+        // Check if is Survival Sandbox game
+        private static bool IsSandbox()
+        {
+            ExperienceModeType xpMode = ExperienceModeManager.GetCurrentExperienceModeType();
+            switch (xpMode)
+            {
+                case ExperienceModeType.Pilgrim:
+                    return true;
+                case ExperienceModeType.Voyageur:
+                    return true;
+                case ExperienceModeType.Stalker:
+                    return true;
+                case ExperienceModeType.Interloper:
+                    return true;
+                case ExperienceModeType.Custom:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private static AnimalType GetAnimalType(SpawnRegion spawnRegion)
+        {
+            AiSubType animalType = spawnRegion.m_AiSubTypeSpawned;
+
+            switch (animalType)
+            {
+                case AiSubType.Wolf:
+                    return AnimalType.Wolf;
+                    break;
+                case AiSubType.Bear:
+                    return AnimalType.Bear;
+                    break;
+                case AiSubType.Stag:
+                    return AnimalType.Deer;
+                    break;
+                case AiSubType.Rabbit:
+                    return AnimalType.Rabbit;
+                    break;
+                case AiSubType.Moose:
+                    return AnimalType.Moose;
+                    break;
+                default:
+                    break;
+            }
+            return AnimalType.False;
+        }
+
+        private static void ApplyAnimalSettings(AnimalType animal, SpawnRegion spawnRegion)
+        {
+            switch (animal)
+            {
+                case AnimalType.Bear:
+                    spawnRegion.m_NumHoursBetweenRespawns *= Settings.settings.bearRespawnRate;
+                    break;
+                case AnimalType.Deer:
+                    spawnRegion.m_NumHoursBetweenRespawns *= Settings.settings.deerRespawnRate;
+                    break;
+                case AnimalType.Moose:
+                    spawnRegion.m_NumHoursBetweenRespawns *= Settings.settings.mooseRespawnRate;
+                    break;
+                case AnimalType.Rabbit:
+                    spawnRegion.m_NumHoursBetweenRespawns *= Settings.settings.mooseRespawnRate;
+                    break;
+                case AnimalType.Wolf:
+                    spawnRegion.m_NumHoursBetweenRespawns *= Settings.settings.wolfRespawnRate;
+                    break;
+                default:
+                    MelonLogger.Msg("ERROR: Animal Type Settings not found!");
+                    return;
+            }
+        }
+
+        private static void ApplyFishSettings(IceFishingHole iceFishingHole)
+        {
+            iceFishingHole.m_MinGameMinutesCatchFish /= Settings.settings.fishRespawnRate;
+            iceFishingHole.m_MaxGameMinutesCatchFish /= Settings.settings.fishRespawnRate;
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Reflection;
+using ModSettings;
+
+namespace RespawnTweaker
+{
+    class RespawnTweakerSettings : JsonModSettings
+    {
+        [Section("Enable Mod")]
+        [Name("Enable Mod")]
+        [Description("NO: Mod is disabled. YES: Mod is enabled.")]
+        public bool modFunction = false;
+
+        [Name("Respawn Rate: Bear")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float bearRespawnRate = 1f;
+
+        [Name("Respawn Rate: Deer")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float deerRespawnRate = 1f;
+
+        [Name("Respawn Rate: Fish")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float fishRespawnRate = 1f;
+
+        [Name("Respawn Rate: Moose")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float mooseRespawnRate = 1f;
+
+        [Name("Respawn Rate: Rabbit")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float rabbitRespawnRate = 1f;
+
+        [Name("Respawn Rate: Wolf")]
+        [Description("MULTIPLIER for Respawn Rate.\n1 = Game Default, 2 = 2x FASTER than Game Default,\n0.5 = 2x SLOWER than Game Default, 0 = Never.")]
+        [Slider(0f, 2f, 201, NumberFormat = "{0:0.00##}x")]
+        public float wolfRespawnRate = 1f;
+
+        protected override void OnChange(FieldInfo field, object oldValue, object newValue)
+        {
+            if (field.Name == nameof(modFunction)) RefreshFields();
+        }
+
+        internal void RefreshFields()
+        {
+            SetFieldVisible(nameof(bearRespawnRate), modFunction);
+            SetFieldVisible(nameof(deerRespawnRate), modFunction);
+            SetFieldVisible(nameof(fishRespawnRate), modFunction);
+            SetFieldVisible(nameof(mooseRespawnRate), modFunction);
+            SetFieldVisible(nameof(rabbitRespawnRate), modFunction);
+            SetFieldVisible(nameof(wolfRespawnRate), modFunction);
+        }
+    }
+
+    internal static class Settings
+    {
+        internal static RespawnTweakerSettings settings;
+        internal static void OnLoad()
+        {
+            settings = new RespawnTweakerSettings();
+            settings.RefreshFields();
+            settings.AddToModSettings("Respawn Tweaker");
+        }
+    } 
+}
